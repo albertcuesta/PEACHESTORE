26cc1c4ff2b5c0de8b83bb9bd088d80f5650dda1
albertcuesta <acm2291@gmail.com>
2014-05-16 18:25:43 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+__author__ = 'Alumne'
+
+from distutils.core import setup
+
+setup(name='PEACHESTORE',
+      version='python 3',
+      author='albert cuesta',
+      author_email='albert_cm_91@hotmail.com',
+      url='https://github.com/albertcuesta/PEACHESTORE',
+      description='es una tienda online de aplicaciones moviles similar a google play',
+      packager=['PEACHSTORE']
+      )
